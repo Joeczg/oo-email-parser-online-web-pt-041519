7ca03330d7ba41c9430e7e074e1b447f812282a9
zhenggao cai <caizhengao1992@gmail.com>
2019-05-18 05:52:08 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,9 +10,9 @@ class EmailParser
     
   end
   def parse
-  @emails.split.collect {|address| address.split(',')} 
-  returnArr = splitEmail.flatten.uniq
-  returnArr
+  new_array= @emails.split.collect {|address| address.split(',')} 
+  new_array = splitEmail.flatten.uniq
+  new_array
   
     
     
